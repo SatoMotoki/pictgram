cf0ae87a03c0125426b5a8822eff6c62a23d754a
Sato_Motoki <sam.moto.com@gmail.com>
2018-11-07 11:33:30 +0900

    no message

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, {
+    presence: true,
+    length: {maximum: 15}
+  }
+  validates :email, {
+    presence: true,
+    format: { with: VALID_EMAIL_REGEX }
+  }
+  validates :password, {
+    length: { in: 8..32 },
+    format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/ }
+  }
+
+  has_secure_password
+end
