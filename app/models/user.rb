911c2a0d1b49df837b4c267eef296ccaae57eac7
Sato_Motoki <sam.moto.com@gmail.com>
2018-11-07 23:45:16 +0900

    バリデーション パスワード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,9 @@ class User < ApplicationRecord
     format: { with: VALID_EMAIL_REGEX }
   }
   validates :password, {
-    length: { in: 8..32 },
-    format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/ }
+    # length: { in: 8..32 },
+    # format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/ },
+    format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
   }
 
   has_secure_password
