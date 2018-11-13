c7b6df3c23ca62716fa7bdae383ff597dac5cdee
Sato_Motoki <sam.moto.com@gmail.com>
2018-11-13 12:25:21 +0900

    ログインフォーム実装 課題提出

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   add_flash_types :success, :info, :warning, :denger
+#######課題 ヘルパーにメソッド共有#########
+  helper_method :current_user, :logged_in?
+#######課題 ヘルパーにメソッド共有#########
+
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+#######課題により削除#########
+  # def current_user
+  #   @current_user ||= User.find_by(id: session[:user_id])
+  # end
+  #
+  # def logged_in?
+  #   !current_user.nil?
+  # end
+#######課題により削除#########
+
 end
